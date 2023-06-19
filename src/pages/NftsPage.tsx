723c1e4c43c6653d12e855b449c959ac3205885b
Nyk <0xnykcd@googlemail.com>
2023-06-19 14:12:27 +0200

    nftspage

--- src/pages/NftsPage.tsx
+++ src/pages/NftsPage.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { NftListRedemption } from "../components/project/NftListRedemption";
 import { NftStats } from "../components/nfts/NftStats";
+import { useWallet } from '@solana/wallet-adapter-react';
+import MyMultiButton from '../components/MyMultiButton';
 
 const Blur1 = styled.div`
   background: linear-gradient(180deg, #e6813e 0%, #00b2ff 100%);
@@ -10,15 +12,22 @@ const Blur1 = styled.div`
 `;
 
 export const NftsPage = () => {
+  const wallet = useWallet()
+
   return (
     <div className="h-full relative mb-40 text-start">
       <Blur1 className="absolute -top-40 -right-40 z-0 opacity-20" />
       <Blur1 className="absolute top-40 right-40 z-0 opacity-10" />
       <Blur1 className="absolute top-80 -right-60 z-0 opacity-20" />
       <section className="my-5 ">
-        <h1 className="text-4xl font-bold mb-16">Your NFTs</h1>
-        <NftStats />
-        <NftListRedemption />
+        <h1 className="text-4xl font-bold mb-16 ">Your NFTs</h1>
+        {wallet.publicKey ? <>
+          <NftStats />
+          <NftListRedemption />
+        </> : <div className='w-full flex items-center justify-center flex-col gap-4'>
+          <p>Please connect your wallet:</p>
+          <MyMultiButton />
+        </div>}
       </section>
     </div>
   );
