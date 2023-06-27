97ce7aad40997ea690cdda83c03bd400e73a64f0
Nyk <0xnykcd@googlemail.com>
2023-06-27 20:55:58 +0200

    wallet modal styling

--- src/components/MyMultiButton.tsx
+++ src/components/MyMultiButton.tsx
@@ -4,7 +4,7 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 export default function MyMultiButton() {
   return (
     <div className="ml-4">
-      <WalletMultiButton className="btn" />
+      <WalletMultiButton className="btn !rounded-none hover:scale-105" />
     </div>
   );
 }
